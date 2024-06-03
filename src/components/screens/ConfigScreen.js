862290d9b627df0cef0801baf84312e7ec8495a5
Patrick Chardavoine <p.chardavoine@monkey-soft.fr>
2024-06-03 10:41:57 +0200

    composant urlInput pr enregistrement adresse url de backend

---
+++ src/components/screens/ConfigScreen.js
@@ -0,0 +1,19 @@
+import { View } from 'react-native';
+import Message from "../Message";
+import FooterLogin from "../FooterLogin";
+import UrlInput from "../UrlInput";
+
+
+const ConfigScreen = ({ navigation }) => {
+    return (
+        <View style={{flex:1, justifyContent: 'flex-start'}}>
+            <Message />
+            <UrlInput />
+            <View style={{ position: 'absolute', left: 0, right: 0, bottom: 0, height: 55, backgroundColor: '#00334A'}}>
+                <FooterLogin/>
+            </View>
+        </View>
+    );
+};
+
+export default ConfigScreen;
